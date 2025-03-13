4b76d30c25fff9baf3b357db86fa5665554e32e7
colaola20 <soryo@farmingdale.edu>
2025-03-12 21:39:03 -0400

    added main scene and connected it to the controller

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Week 3: Trivia
- 
+# ios101-project3-trivia-starter
---
+++ Trivia/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Olha Sorych on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var qustionTextView: UITextView!
+    
+    @IBOutlet weak var answ1Button: UIButton!
+    
+    @IBOutlet weak var answ2Button: UIButton!
+    
+    @IBOutlet weak var answ3Button: UIButton!
+    
+    @IBOutlet weak var answr4Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
